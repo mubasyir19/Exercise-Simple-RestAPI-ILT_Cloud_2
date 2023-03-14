7c6916c937e5758bd38421fff5b8ec4934091ac6
mubasyir19 <m.mubasyir19@gmail.com>
2023-03-14 16:16:15 +0700

    Update source code native

--- Native/index.js
+++ Native/index.js
@@ -1,48 +1,66 @@
 const http = require("http");
+const url = require("url");
+
 const contacts = require("./contacts");
 
-(async () => {
-  const server = http.createServer((request, response) => {
-    response.setHeader("Content-Type", "application/json");
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const endpoint = parsedUrl.pathname;
+  const query = parsedUrl.query;
 
-    const { url } = request;
-
-    if (url === "/contacts") {
-      const { method } = request;
-
-      if (method === "POST") {
-        let body = "";
-
-        request.on("data", (chunk) => {
-          body += chunk.toString();
-        });
-        response.statusCode = 200;
-        return response.end(
-          JSON.stringify({
-            data: body,
-          })
-        );
+  if (endpoint === "/contacts" && req.method === "GET") {
+    if (query.id) {
+      const dataObj = contacts.find((obj) => obj.id === parseInt(query.id));
+      if (dataObj) {
+        res.setHeader("Content-Type", "application/json");
+        res.statusCode = 200;
+        res.end(JSON.stringify(dataObj));
       } else {
-        response.statusCode = 400;
-        return response.end(
-          JSON.stringify({
-            message: "Bad request",
-            data: [],
-          })
-        );
+        res.statusCode = 404;
+        res.end("Data object not found");
       }
     } else {
-      response.statusCode = 404;
-      return response.end(
-        JSON.stringify({
-          message: "Endpoint not found",
-          data: [],
-        })
-      );
+      res.setHeader("Content-Type", "application/json");
+      res.statusCode = 200;
+      res.end(JSON.stringify(contacts));
     }
-  });
+  } else if (endpoint === "/contacts" && req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      const newData = JSON.parse(body);
+      newData.id = contacts.length + 1;
+      contacts.push(newData);
+      res.setHeader("Content-Type", "application/json");
+      res.statusCode = 201;
+      res.end(JSON.stringify(newData));
+    });
+  } else if (endpoint === "/contacts" && req.method === "DELETE") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      const deletedData = JSON.parse(body);
+      const index = contacts.findIndex((obj) => obj.id === deletedData.id);
+      if (index >= 0) {
+        contacts.splice(index, 1);
+        res.setHeader("Content-Type", "application/json");
+        res.statusCode = 200;
+        res.end(JSON.stringify(deletedData));
+      } else {
+        res.statusCode = 404;
+        res.end("Data object not found");
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end("Endpoint not found");
+  }
+});
 
-  server.listen(3000, "localhost", () => {
-    console.log("Server running on http://localhost:3000");
-  });
-})();
+server.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
